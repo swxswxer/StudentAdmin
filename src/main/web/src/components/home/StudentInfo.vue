79fb19bba1742c30d4b17bf9080ceff982250df4
Superyong <269085434@qq.com>
2022-12-20 17:55:10 +0800

    remove student page scroll bar

--- src/main/web/src/components/home/StudentInfo.vue
+++ src/main/web/src/components/home/StudentInfo.vue
@@ -13,56 +13,58 @@
         </el-col>
       </el-row>
     </div>
-    <el-table
-        stripe
-        highlight-current-row
-        style="width: 100%"
-        :show-header="showHeader"
-        :loading="tableLoading"
-        :data="tableData"
-        :height="tableHeight">
-      <el-table-column
-          prop="studentid"
-          label="学号"
-      >
-      </el-table-column>
-      <el-table-column
-          prop="name"
-          label="姓名"
-      >
-        <template slot-scope="scope">
-          <el-tag size="medium">{{ scope.row.name }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column
-          prop="age"
-          label="年龄"
-      >
-      </el-table-column>
-      <el-table-column
-          prop="sex"
-          label="性别"
-      >
-      </el-table-column>
-      <el-table-column
-          prop="major"
-          label="专业"
-      >
-      </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">编辑
-          </el-button>
-          <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)">删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div class="table">
+      <el-table
+          stripe
+          highlight-current-row
+          style="width: 100%"
+          :show-header="showHeader"
+          :loading="tableLoading"
+          :data="tableData"
+          :height="tableHeight">
+        <el-table-column
+            prop="studentid"
+            label="学号"
+        >
+        </el-table-column>
+        <el-table-column
+            prop="name"
+            label="姓名"
+        >
+          <template slot-scope="scope">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="age"
+            label="年龄"
+        >
+        </el-table-column>
+        <el-table-column
+            prop="sex"
+            label="性别"
+        >
+        </el-table-column>
+        <el-table-column
+            prop="major"
+            label="专业"
+        >
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑
+            </el-button>
+            <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -116,7 +118,7 @@ export default {
 <style scoped>
 .studentInfo {
   height: 100%;
-  width: 100%;
+  width: calc(100% - 5px);
 }
 
 .tableHeader {
