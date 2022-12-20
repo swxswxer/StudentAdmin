611f55d423379571a375d0f70fc523b94e8571f5
Superyong <269085434@qq.com>
2022-12-20 17:51:53 +0800

    add student info search

--- src/main/web/src/components/home/StudentInfo.vue
+++ src/main/web/src/components/home/StudentInfo.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>
+  <div id="studentInfo" class="studentInfo">
+    <div id="tableHeader" class="tableHeader">
+      <el-row type="flex" justify="start" align="middle" :gutter="5" style="height: 100%; margin-left: 10px">
+        <el-col :span="3">
+          <el-input v-model="nameInput" placeholder="请输入学生姓名"></el-input>
+        </el-col>
+        <el-col :span="3">
+          <el-input v-model="majorInput" placeholder="请输入专业"></el-input>
+        </el-col>
+        <el-col :span="3" :pull="1" style="margin-left: 10px">
+          <el-button type="primary" size="medium" @click="searchClick" :loading="searchLoading">查询</el-button>
+        </el-col>
+      </el-row>
+    </div>
     <el-table
         stripe
         highlight-current-row
         style="width: 100%"
-        :loading="loading"
+        :show-header="showHeader"
+        :loading="tableLoading"
         :data="tableData"
         :height="tableHeight">
       <el-table-column
@@ -59,33 +73,56 @@ export default {
   name: "StudentInfo",
   data() {
     return {
-      loading: false,
+      tableLoading: false,
+      showHeader: false,
+      searchLoading: false,
+      nameInput: '',
+      majorInput: '',
       tableHeight: 0,
       tableData: [],
     }
   },
   created() {
-    this.tableHeight = window.innerHeight;
-
-    this.loading = true
+    this.$nextTick(() => {
+      this.tableHeight = document.getElementById("studentInfo").offsetHeight - document.getElementById("tableHeader").offsetHeight
+    })
+    this.tableLoading = true
     getStudentInfo().then(res => {
       if (res.status === true) {
         this.tableData = res.data
       }
-      this.loading = false
+      if (this.tableData.length === 0) {
+        this.showHeader = false
+      }
+      this.tableLoading = false
     })
   },
   methods: {
     handleEdit(index, row) {
-      console.log("edit index: " + index + "row: " + row)
+      console.log("edit index: " + index + " row: " + row)
     },
     handleDelete(index, row) {
-      console.log("edit index: " + index + "row: " + row)
+      console.log("edit index: " + index + " row: " + row)
+    },
+    searchClick() {
+      console.log("name: " + this.nameInput + " major: " + this.majorInput)
+      this.searchLoading = true
+      this.searchLoading = false
     }
   }
 }
 </script>
 
 <style scoped>
+.studentInfo {
+  height: 100%;
+  width: 100%;
+}
+
+.tableHeader {
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid #EBEEF5;
+}
 
 </style>
