c6f173419653ff226672ad1330337c8adf717a1f
Superyong <269085434@qq.com>
2022-12-21 21:17:22 +0800

    add new api dial

---
+++ src/main/web/src/mock/server.js
@@ -0,0 +1,37 @@
+let express  = require('express');
+let app = express()
+let router = express.Router();
+let mock = require('mockjs');
+let bodyParser = require('body-parser')
+
+let mockLogin = require("./mock_login.json")
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials","true");
+    res.header("X-Powered-By",' 3.2.1')
+    if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+});
+
+router.use("/student", require("./student"))
+app.use("/stu-admin", router)
+app.post("/stu-admin/login", function (req, res) {
+    let logInData = req.body
+    console.log(logInData.username)
+    let userName = logInData.username
+    if (userName === "root1") {
+        res.json(mock.mock(mockLogin.success_op))
+    } else {
+        res.json(mock.mock(mockLogin.error_op))
+    }
+})
+
+app.listen(8081, function () {
+    console.log("mock server start!")
+})
